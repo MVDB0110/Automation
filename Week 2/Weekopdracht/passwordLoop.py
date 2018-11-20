ecae53e5dd3e85ff22d48aa37be4f9b6d0a6bc65
Mike van den Brink <mike.vandenbrink@student.hu.nl>
2018-11-20 11:14:06 +0100

    Week 2 - Weekopdracht af

--- Week 2/Weekopdracht/passwordLoop.py
+++ Week 2/Weekopdracht/passwordLoop.py
@@ -24,4 +24,6 @@ def SHAloop():
         char = chr(i)
         char = char.encode()
         if hash == hashlib.sha224(char).hexdigest():
-            print("Het wachtwoord is gevonden.")
+            print("Het wachtwoord is gevonden", char.decode(), "is de hoofdletter.")
+
+SHAloop()
