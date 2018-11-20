15e1816c85c252a238c810f9a7055791b215b881
Mike van den Brink <mike.vandenbrink@student.hu.nl>
2018-11-20 11:02:13 +0100

    Week 2 - Weekopdracht af

--- Week 2/Weekopdracht/passwordLoop.py
+++ Week 2/Weekopdracht/passwordLoop.py
@@ -1,17 +1,19 @@
 import hashlib
+import sys
 
 p = "326f13e25448c99ccae78b0afa1c84e6fae2d8a587976df3a86628ae"
 
-for i in range(100):
+for i in range(65,90):
     letter1 = chr(i)
-    for i2 in range(100):
+    for i2 in range(65,90):
         letter2 = chr(i2)
-        for i3 in range(100):
+        for i3 in range(65,90):
             letter3 = chr(i3)
             ww = letter1 + letter2 + letter3
+            print(ww)
             if hashlib.sha224(ww.encode()).hexdigest() == p:
                 print("Je hebt het wachtwoord gevonden! Het was:",ww)
-                break
+                sys.exit()
 
             else:
-                print("Helaas, nog een poging (poging",i,")")
+                print("Helaas, nog een poging")
