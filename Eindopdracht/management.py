5b23c88d65af48fa73efdd77092f542668692d6a
Mike van den Brink <mike.vandenbrink@student.hu.nl>
2018-12-14 18:02:54 +0100

    SQLite implementatie en een keer hostname opslaan.

--- Eindopdracht/management.py
+++ Eindopdracht/management.py
@@ -1,19 +1,23 @@
 import socket
 from _thread import start_new_thread
 import json
+import sqlite3
 
 def new_host(csocket,addr):
     print('> Verbonden met ' + addr[0] + ':' + str(addr[1]))
     while True:
-        file = open('usage.txt','a')
+        db = sqlite3.connect('usage.sqlite')
+        dbconn = db.cursor()
         data = conn.recv(1024)
         try:
             data = json.loads(data.decode('ascii'))
         except:
             break;
-        file.write(str(data[0])+';'+str(data[1])+';'+str(data[2])+';'+str(data[3])+';'+str(data[4])+';'+str(data[5])+'\n')
-        file.close()
 
+        dbconn.execute('DELETE FROM computerusage WHERE hostname=?',(str(data[1]),))
+        dbconn.execute('INSERT INTO computerusage VALUES(?,?,?,?,?,?)', (data[0],str(data[1]),data[2],data[3],data[4],data[5]))
+        db.commit()
+        db.close()
     csocket.close()
 
 HOST = '' # Alle beschikbare interfaces
@@ -26,6 +30,12 @@ try:
     print('> Socket luistert op poort:',PORT)
 except:
     print("> Port is in gebruik.")
+
+db = sqlite3.connect('usage.sqlite')
+dbconn = db.cursor()
+dbconn.execute('CREATE TABLE IF NOT EXISTS computerusage (stamp REAL,hostname VARCHAR(30),cpercent REAL,mtotal REAL,musage REAL,mpercent REAL);')
+db.commit()
+db.close()
 
 while True:
     # Wacht op connecties (blocking)
