4ff48ed257fb082c446a260c7bf2913f26fc56e7
Mike van den Brink <mike.vandenbrink@student.hu.nl>
2018-12-14 19:02:00 +0100

    Matplotlib informatie tonen uit sqlite

--- Eindopdracht/cgi.py
+++ Eindopdracht/cgi.py
@@ -1,15 +1,26 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import sqlite3
+
+minions = []
+percentage = []
+
+db = sqlite3.connect('usage.sqlite')
+dbconn = db.cursor()
+dbconn.execute('SELECT * FROM computerusage')
+fetch = dbconn.fetchall()
+for line in fetch:
+    minions.append(line[1])
+    percentage.append(line[5])
 
 plt.rcdefaults()
 fig, ax = plt.subplots()
 
 # Example data
-minions = ('Minion1','Minion2')
-ybar = np.arange(len(minions))
-xbar = (percentage, 20)
 
-ax.barh(minions, xbar, align='center',color='red', ecolor='black')
+ybar = np.arange(len(minions))
+
+ax.barh(minions, percentage, align='center',color='red', ecolor='black')
 ax.set_yticks(ybar)
 ax.set_yticklabels(minions)
 ax.set(xlim=[0,100])
