1f25f912bf102cd18cd2237975734836c8affead
Mike van den Brink <mike.vandenbrink@student.hu.nl>
2018-12-16 18:40:47 +0100

    Matplotlib memory en cpu in plot

--- Eindopdracht/cgi.py
+++ Eindopdracht/cgi.py
@@ -3,7 +3,8 @@ import numpy as np
 import sqlite3
 
 minions = []
-percentage = []
+mempercent = []
+cpupercent = []
 
 db = sqlite3.connect('usage.sqlite')
 dbconn = db.cursor()
@@ -11,16 +12,15 @@ dbconn.execute('SELECT * FROM computerusage')
 fetch = dbconn.fetchall()
 for line in fetch:
     minions.append(line[1])
-    percentage.append(line[5])
+    mempercent.append(line[5])
+    cpupercent.append(line[2])
 
 plt.rcdefaults()
 fig, ax = plt.subplots()
 
-# Example data
-
 ybar = np.arange(len(minions))
 
-ax.barh(minions, percentage, align='center',color='red', ecolor='black')
+ax.barh(minions, mempercent, align='center',color='red', ecolor='black')
 ax.set_yticks(ybar)
 ax.set_yticklabels(minions)
 ax.set(xlim=[0,100])
@@ -28,4 +28,19 @@ ax.invert_yaxis()  # labels read top-to-bottom
 ax.set_xlabel('Percentage')
 ax.set_title('Memory Usage')
 
-plt.show()
+plt.savefig('memory.png')
+
+plt.rcdefaults()
+fig2, ax2 = plt.subplots()
+
+ybar = np.arange(len(minions))
+
+ax2.barh(minions, cpupercent, align='center',color='red', ecolor='black')
+ax2.set_yticks(ybar)
+ax2.set_yticklabels(minions)
+ax2.set(xlim=[0,100])
+ax2.invert_yaxis()  # labels read top-to-bottom
+ax2.set_xlabel('Percentage')
+ax2.set_title('CPU Usage')
+
+plt.savefig('cpu.png')
