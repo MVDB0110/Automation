8166636c50d5aa5eabace08da0cf2f5c0827ea4b
Mike van den Brink <mike.vandenbrink@student.hu.nl>
2019-01-11 20:23:48 +0100

    Fout CGI

--- Eindopdracht/Management/cgi.py
+++ Eindopdracht/Management/cgi.py
@@ -44,7 +44,7 @@ for line in fetch:
     mempercent.append(line[3]) # Voeg percentage geheugengebruik toe aan mempercent
     cpupercent.append(line[2]) # Voeg percentage processorgebruik toe aan cpupercent
     diskpercent.append(line[6]) # Voeg percentage hardeschijf toe aan diskpercent
-    htmlList.append((line[0],line[1],line[2],round(line[3]/1024**3,2),round(line[4]/1024**3,2),line[5]))
+    htmlList.append((line[0],line[1],line[2],line[3],round(line[4]/1024**3,2),round(line[5]/1024**3,2)))
 
 cpu = HorizontalBar('CPU Usage','Percentage',minions,cpupercent,'red')
 memory = HorizontalBar('Memory Usage','Percentage',minions,mempercent,'red')
@@ -65,13 +65,21 @@ print("<img src='memory.png'>\n")
 print("<img src='cpu.png'>\n")
 print("<img src='disk.png'>\n")
 print("</div>\n")
-print("<table border='1'>\n")
+print("<table align='center' border='1'>\n")
 print("<tr>\n")
 print("<td align ='center'>Minion</td><td align ='center'>CPU usage</td><td align ='center'>Memory Usage</td><td align ='center'>Disk Total</td><td align ='center'>Disk Usage</td><td align ='center'>Time Stamp</td>\n")
 print("</tr>\n")
 for minion in htmlList:
+    if minion[2] > 89:
+        ccolor='#FF0000' # Rood
+    else:
+        ccolor='#00FF00' # Groen
+    if minion[3] > 89:
+        mcolor='#FF0000' # Rood
+    else:
+        mcolor='#00FF00' # Groen
     print("<tr>\n")
-    print("<td align ='center'>"+str(minion[1])+"</td><td align ='center'>"+str(minion[2])+"</td><td align ='center'>"+str(minion[3])+"</td><td align ='center'>"+str(minion[4])+"</td><td align ='center'>"+str(minion[5])+"</td><td align ='center'>"+str(minion[0])+"</td>\n")
+    print("<td align ='center'>"+str(minion[1])+"</td><td bgcolor='"+ccolor+"' align ='center'>"+str(minion[2])+"</td><td bgcolor='"+mcolor+"' align ='center'>"+str(minion[3])+"</td><td align ='center'>"+str(minion[4])+"</td><td align ='center'>"+str(minion[5])+"</td><td align ='center'>"+str(minion[0])+"</td>\n")
     print("</tr>\n")
 print("</table>\n")
 print("</body>\n")
