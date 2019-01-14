b273474b259bd9c0c7f01b653e8addc62c4b5734
Mike van den Brink <mike.vandenbrink@student.hu.nl>
2019-01-14 12:23:47 +0100

    interval verhoogd cpu

--- Eindopdracht/Client/clientclasses.py
+++ Eindopdracht/Client/clientclasses.py
@@ -6,7 +6,7 @@ class ComputerUsage():
     def __init__(self):
         memorydump = psutil.virtual_memory() # Vraag memory gegevens op
         self.mempercentage = memorydump[2] # Vraag percentage van het memory dat in gebruik is
-        self.cpupercentage = psutil.cpu_percent(interval=0.4,percpu=False) # Vraag het percentage van de gehele CPU over een interval van 0.4
+        self.cpupercentage = psutil.cpu_percent(interval=0.9,percpu=False) # Vraag het percentage van de gehele CPU over een interval van 0.4
         self.hostname = socket.gethostname() # Vraag hostname van de minion op
         self.stamp = time.time() # Vraag timestamp op
         self.disktotal = psutil.disk_usage(".").total # Vraag het totale schijf volume op
